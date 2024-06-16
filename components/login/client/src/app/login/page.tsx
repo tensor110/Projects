e8093ca83b6ad0d6c7c11940cd4d77c0cec65f7d
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-06-16 17:24:39 +0530

    init

--- components/login/client/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from '../../components/login'
-
-export default function Home() {
-  return (
-    <main className="flex w-screen h-screen items-center justify-center bg-white text-black">
-      <Login/>
-    </main>
-  );
-}
--- components/login/client/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex w-screen h-screen items-center justify-center bg-white text-black">
-      <div>Home</div>
-    </main>
-  );
-}
